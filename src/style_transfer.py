1f84a8c5b1b347b80d05be1e7d9379d2d713b00b
Josh Mantovani <josh.mantova@gmail.com>
2021-03-02 18:01:01 -0700

    Working version of style transfer code

--- src/style_transfer.py
+++ src/style_transfer.py
@@ -10,6 +10,7 @@ import torchvision.transforms as transforms
 import torchvision.models as models
 
 import copy
+import time
 
 class ContentLoss(nn.Module):
 
@@ -48,9 +49,13 @@ def gram_matrix(input):
     G = torch.mm(features, features.t())
     return G.div(a * b * c * d)
 
+def get_input_optimizer(input_img):
+    optimizer = optim.LBFGS([input_img.requires_grad_()])
+    return optimizer
+
 def get_style_model_and_losses(cnn, normalization_mean, normalization_std, style_img, content_img,
                                 content_layers=['conv_4'], 
-                                style_layers_default=['conv_1', 'conv_2', 'conv_3', 'conv_4', 'conv_5']):
+                                style_layers=['conv_1', 'conv_2', 'conv_3', 'conv_4', 'conv_5']):
     cnn = copy.deepcopy(cnn)
 
     normalization = Normalization(normalization_mean, normalization_std).to(device)
@@ -76,6 +81,72 @@ def get_style_model_and_losses(cnn, normalization_mean, normalization_std, style
 
         model.add_module(name, layer)
 
+        if name in content_layers:
+            target = model(content_img).detach()
+            content_loss = ContentLoss(target)
+            model.add_module(f"content_loss_{i}", content_loss)
+            content_losses.append(content_loss)
+
+        if name in style_layers:
+            target_feature = model(style_img).detach()
+            style_loss = StyleLoss(target_feature)
+            model.add_module(f"style_loss_{i}", style_loss)
+            style_losses.append(style_loss)
+
+    for i in range(len(model) - 1, -1, -1):
+        if isinstance(model[i], ContentLoss) or isinstance(model[i], StyleLoss):
+            break
+
+    model = model[:(i + 1)]
+
+    return model, style_losses, content_losses
+
+def run_style_transfer(cnn, normalization_mean, normalization_std, content_img, style_img,
+                        input_img, num_steps=300, style_weight=1e6, content_weight=1):
+    print("Building the style transfer model..")
+    model, style_losses, content_losses = get_style_model_and_losses(cnn, normalization_mean, normalization_std,
+                                                                    style_img, content_img)
+    optimizer = get_input_optimizer(input_img)
+
+    print('Optimizing..')
+    run = [0]
+    while run[0] <= num_steps:
+
+        def closure():
+            input_img.data.clamp_(0, 1)
+
+            optimizer.zero_grad()
+            model(input_img)
+            style_score = 0
+            content_score = 0
+
+            for sl in style_losses:
+                style_score += sl.loss
+
+            for cl in content_losses:
+                content_score += cl.loss
+
+            style_score *= style_weight
+            content_score *= content_weight
+
+            loss = style_score + content_score
+            loss.backward()
+
+            run[0] += 1
+            if run[0] % 50 == 0:
+                print(f"run {run}:")
+                print(f"Style Loss: {style_score.item():4f} Content Loss: {content_score.item():4f}")
+                print()
+
+            return style_score + content_score
+
+        optimizer.step(closure)
+
+    input_img.data.clamp_(0, 1)
+
+    return input_img
+
+
 def image_loader(image_name):
     image = Image.open(image_name)
     image = loader(image).unsqueeze(0)
@@ -91,8 +162,12 @@ def imshow(tensor, title=None):
     plt.pause(0.001)
 
 if __name__ == "__main__":
+    start = time.time()
+    # plt.ion()
+    plt.ioff()
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    imsize = 512 if torch.cuda.is_available() else 128
+    # imsize = 512 if torch.cuda.is_available() else 128
+    imsize = 512
     loader = transforms.Compose([
         transforms.Resize(imsize),
         transforms.ToTensor()
@@ -104,7 +179,6 @@ if __name__ == "__main__":
     assert style_img.size() == content_img.size()
 
     unloader = transforms.ToPILImage()
-    plt.ion()
 
     plt.figure()
     imshow(style_img, title='Style Image')
@@ -117,3 +191,17 @@ if __name__ == "__main__":
     cnn_normalization_mean = torch.tensor([0.485, 0.456, 0.406]).to(device)
     cnn_normalization_std = torch.tensor([0.229, 0.224, 0.225]).to(device)
 
+    input_img = content_img.clone()
+    # input_img = torch.randn(content_img.data.size(), device=device)
+
+    plt.figure()
+    imshow(input_img, title='Input Image')
+
+    output = run_style_transfer(cnn, cnn_normalization_mean, cnn_normalization_std,
+                                content_img, style_img, input_img)
+    end = time.time()
+    print(f"Time taken to train: {(end-start)/60} minutes")
+    plt.figure()
+    imshow(output, title='Output Image')
+    plt.savefig('Styled input image.png')
+    plt.show()
