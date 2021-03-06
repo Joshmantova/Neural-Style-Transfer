7646ca5a830f00b278de1f38ed2fc2efd91ed196
Josh Mantovani <josh.mantova@gmail.com>
2021-03-05 22:29:29 -0700

    Changing website to use placeholders that update for image rather than just showing one image

--- src/style_transfer_website.py
+++ src/style_transfer_website.py
@@ -8,13 +8,14 @@ from style_transfer import (image_loader,
 from PIL import Image
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-imsize = 512 if torch.cuda.is_available() else 128
+# imsize = 512 if torch.cuda.is_available() else 128
+imsize = 512
 loader = transforms.Compose([
     transforms.Resize(imsize),
     transforms.ToTensor()
 ])
 style_image_name = 'Alex_Grey_Over_Soul.jpg'
-content_image_name = '../../../../downloads/bird.jpg'
+content_image_name = '../imgs/bird.jpg'
 style_image_pil = Image.open(f'../imgs/{style_image_name}')
 content_image_pil = Image.open(content_image_name)
 st.image(style_image_pil)
@@ -30,14 +31,20 @@ cnn_normalization_mean = torch.tensor([0.485, 0.456, 0.406]).to(device)
 cnn_normalization_std = torch.tensor([0.229, 0.224, 0.225]).to(device)
 
 st.write("Running Style Transfer")
-input_img = content_img.clone()
-output = run_style_transfer(cnn, cnn_normalization_mean, cnn_normalization_std,
-                            content_img, style_img, input_img, style_weight=1e4)
-# out = st.empty()
-# for i in run_style_transfer(cnn, cnn_normalization_mean, cnn_normalization_std,
-#                             content_img, style_img, input_img, style_weight=1e4):
-#     out.image(imshow(i.resize((444,444))))
+# input_img = content_img.clone()
+input_img = torch.randn(content_img.data.size(), device=device)
+
+# output = run_style_transfer(cnn, cnn_normalization_mean, cnn_normalization_std,
+#                             content_img, style_img, input_img, style_weight=1e4)
+out = st.empty()
+im = imshow(input_img)
+out.image(im.resize((444,444)))
+for i in run_style_transfer(cnn, cnn_normalization_mean, cnn_normalization_std,
+                            content_img, style_img, input_img, style_weight=1e4):
+    im = imshow(i)
+    out.image(im.resize((444,444)))
+    # out.image(imshow(i.resize((444,444))))
 
 # out = st.empty()
 # out.image(imshow(output).resize((444,444)))
-st.image(imshow(output).resize((444,444)))
+# st.image(imshow(output).resize((444,444)))
