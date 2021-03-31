206579ef6b875291d16295a047bc635d6b9ff0a6
Josh Mantovani <josh.mantova@gmail.com>
2021-03-30 19:51:02 -0600

    Allowing for selection of preset style images

--- src/style_transfer_website.py
+++ src/style_transfer_website.py
@@ -7,12 +7,20 @@ from style_transfer import (image_loader,
                             imshow)
 from PIL import Image
 from io import BytesIO
+import os
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 st.write(f'Running on: {device}')
 image_quality_selection = st.sidebar.selectbox('Image Quality',
                                                 ["Low", "Medium", "High", "Ultra High"])
-style_weight = st.sidebar.slider('Style Weight', 3, 7, step=1, value=4)
+
+preset_style_image_names = os.listdir("imgs/style_images")
+
+#TODO: create style weight and iterations dictionary based on each style image for general best results
+
+style_image_selection = st.sidebar.selectbox("Style Image",
+                                                preset_style_image_names) #first default should probably be starry night
+style_weight = st.sidebar.slider('Style Weight', 2, 7, step=1, value=4)
 number_of_iterations = st.sidebar.slider('Number of Iterations', 150, 1000, 300, 50)
 
 image_quality_mapping = {"Low": 128, "Medium": 256, "High": 512, "Ultra High": 1024}
@@ -21,7 +29,7 @@ loader = transforms.Compose([
     transforms.Resize(imsize),
     transforms.ToTensor()
 ])
-default_style_image_name = 'over_soul.jpg'
+default_style_image_name = preset_style_image_names[0]
 default_content_image_name = 'bird.jpg'
 
 style_image_upload = st.file_uploader('Style Image')
@@ -30,26 +38,26 @@ content_image_upload = st.file_uploader('Content Image')
 if style_image_upload:
     style_image = Image.open(style_image_upload)
 else:
-    style_image = Image.open(f"imgs/{default_style_image_name}")
+    style_image = Image.open(f"imgs/style_images/{default_style_image_name}")
 
 if content_image_upload:
     content_image = Image.open(content_image_upload)
 else:
     content_image = Image.open(f"imgs/{default_content_image_name}")
 
-content_image_size = content_image.size
-content_image_ratio = content_image_size[0] / content_image_size[1]
-content_image_size_display = int(content_image_ratio * 444)
-
 style_image_size = style_image.size
 style_image_ratio = style_image_size[0] / style_image_size[1]
 style_image_size_display = int(style_image_ratio * 444)
 
-st.title('Content Image')
-st.image(content_image.resize((content_image_size_display,444)))
+content_image_size = content_image.size
+content_image_ratio = content_image_size[0] / content_image_size[1]
+content_image_size_display = int(content_image_ratio * 444)
 
 st.title('Style Image')
 st.image(style_image.resize((style_image_size_display,444)))
+
+st.title('Content Image')
+st.image(content_image.resize((content_image_size_display,444)))
 
 style_image_loader = image_loader(style_image, imsize, device)
 content_image_loader = image_loader(content_image, imsize, device)
@@ -63,21 +71,16 @@ input_img = torch.randn(content_image_loader.data.size(), device=device)
 
 go = st.button('Optimize this image')
 
-out = st.empty()
-iter_info = st.empty()
+stylized_image_placeholder = st.empty()
+iter_placeholder = st.empty()
 
 im = imshow(input_img)
-out.image(im.resize((content_image_size_display,444)))
+stylized_image_placeholder.image(im.resize((content_image_size_display,444)))
 
 if go:
     for i, n in run_style_transfer(cnn, cnn_normalization_mean, cnn_normalization_std,
                                 content_image_loader, style_image_loader, input_img,
                                 num_steps=number_of_iterations-20, style_weight=10 ** style_weight):
         im = imshow(i)
-        out.image(im.resize((content_image_size_display,444)))
-        iter_info.write(f'Iteration Number: {n}')
-    # out.image(imshow(i.resize((444,444))))
-
-# out = st.empty()
-# out.image(imshow(output).resize((444,444)))
-# st.image(imshow(output).resize((444,444)))
+        stylized_image_placeholder.image(im.resize((content_image_size_display,444)))
+        iter_placeholder.write(f'Iteration Number: {n}')
